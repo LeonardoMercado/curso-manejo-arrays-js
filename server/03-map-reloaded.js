7aa463585f74de57bb889edba5507312151888a9
Leonardo Mercado <lfmercadobe@unal.edu.co>
2022-11-27 22:09:42 -0500

    Se desarrolla el ejemplo de map reloaded

---
+++ server/03-map-reloaded.js
@@ -0,0 +1,57 @@
+document.addEventListener("DOMContentLoaded", function(event){
+  
+  const orders = [
+    {
+      customerName: 'Nicolas',
+      total: 60,
+      delivered: true,
+    },
+    {
+      customerName: 'Zulema',
+      total: 120,
+      delivered: false,
+    },
+    {
+      customerName: 'Santiago',
+      total: 180,
+      delivered: true,
+    },
+    {
+      customerName: 'Valentina',
+      total: 240,
+      delivered: true,
+    }
+  ];
+
+  const totalOrders = orders.map(order => order.total);
+  //console.log(totalOrders);
+
+  // const ordersCustom = orders.map(order => {
+  //   order.tax = 0.19;
+  //   return order;
+  // });
+
+  const ordersCustom = orders.map(order => {
+    return {
+      ...order,
+      tax: 0.19,
+    };
+  });
+
+  console.log('Original orders:');
+  console.log(orders);
+  console.log('ordersCustom');
+  console.log(ordersCustom);
+   
+  
+  
+  
+  const total = sumarArray(totalOrders);
+  console.log(total);
+});
+
+function sumarArray(array){
+  let contador = 0;
+  array.forEach(element => contador += element);
+  return contador;
+}
